d484d01e390035d77111ee63c146590db55bb98a
chokingvictim <markrizzo22@gmail.com>
2013-01-16 16:38:53 -0500

    Merge

---
+++ OpenSauce/Halo1/Halo1_CE/Game/AI.cpp
@@ -0,0 +1,111 @@
+/*
+	Yelo: Open Sauce SDK
+		Halo 1 (CE) Edition
+
+	See license\OpenSauce\Halo1_CE for specific license information
+*/
+#include "Common/Precompile.hpp"
+#include "Game/AI.hpp"
+#include "Objects/Objects.hpp"
+#include "Objects/Units.hpp"
+#include "TagGroups/TagGroups.hpp"
+
+#include "TagGroups/project_yellow_definitions.hpp"
+
+#include "Memory/MemoryInterface.hpp"
+
+namespace Yelo
+{
+	namespace AI
+	{
+#define __EL_INCLUDE_ID			__EL_INCLUDE_GAME
+#define __EL_INCLUDE_FILE_ID	__EL_GAME_AI
+#include "Memory/_EngineLayout.inl"
+		
+		s_ai_globals_data* AIGlobals()								DPTR_IMP_GET(ai_globals);
+		t_actor_data* Actors()										DPTR_IMP_GET(actors);
+		t_swarm_data* Swarms()										DPTR_IMP_GET(swarms);
+		t_swarm_component_data* SwarmComponents()					DPTR_IMP_GET(swarm_components);
+		t_prop_data* Props()										DPTR_IMP_GET(props);
+		t_encounter_data* Encounters()								DPTR_IMP_GET(encounters);
+		s_squad_data* Squads()										DPTR_IMP_GET(squads);
+		s_platoon_data* Platoons()									DPTR_IMP_GET(platoons);
+		t_ai_pursuit_data* AIPursuits()								DPTR_IMP_GET(ai_pursuits);
+
+		//ai_communication_dialogue_data
+
+		s_ai_communication_replies_data* AICommunicationReplies()	DPTR_IMP_GET(ai_communication_replies);
+		t_ai_conversation_data* AIConversations()					DPTR_IMP_GET(ai_conversations);
+
+
+		void Initialize()
+		{
+#if !PLATFORM_DISABLE_UNUSED_CODE
+			Memory::CreateHookRelativeCall(&AI::Update, GET_FUNC_VPTR(AI_UPDATE_HOOK), Enums::_x86_opcode_retn);
+#endif
+			static const byte k_null_bytes[7] = {
+                    Enums::_x86_opcode_nop, Enums::_x86_opcode_nop,
+                    Enums::_x86_opcode_nop, Enums::_x86_opcode_nop,
+                    Enums::_x86_opcode_nop, Enums::_x86_opcode_nop,
+                    Enums::_x86_opcode_nop
+            };
+
+            Memory::WriteMemory(GET_FUNC_VPTR(ACTOR_ACTION_HANDLE_VEHICLE_EXIT_HOOK), k_null_bytes, sizeof(k_null_bytes));
+			Memory::WriteRelativeCall(&ActorActionHandleVehicleExitHook, GET_FUNC_VPTR(ACTOR_ACTION_HANDLE_VEHICLE_EXIT_HOOK), true);
+		}
+
+		void Dispose()
+		{
+		}
+
+		void PLATFORM_API Update()
+		{
+		}
+
+		API_FUNC_NAKED void PLATFORM_API ActorActionHandleVehicleExitHook()
+		{
+			__asm {
+				pushad
+				push	ecx		// datum_index unit_index
+				call	ActorActionHandleVehicleExitBoardingSeat
+				popad
+
+				retn
+			}
+		}
+
+		void ActorActionHandleVehicleExitBoardingSeat(datum_index unit_index)
+		{
+			Objects::s_unit_datum* unit = (*Objects::ObjectHeader())[unit_index]->_unit;
+
+			datum_index parent_unit_index = unit->object.parent_object_index;
+			int16 seat_index = unit->unit.vehicle_seat_index;
+
+			TagGroups::s_unit_external_upgrades const* unit_upgrades_definition = 
+				Objects::Units::DefinitionFindUnitUpgradesBlock(parent_unit_index);
+
+			// Check if a unit upgrades definition exists for the vehicle the actor is in
+			if (unit_upgrades_definition != NULL)
+			{
+				// Check if the seat the actor is in is being boarded
+				for (int i = 0; i < unit_upgrades_definition->boarding_seats.Count; i++)
+				{
+					int16 boarding_seat_index = unit_upgrades_definition->boarding_seats[i].seat_index;
+					int16 target_seat_index = unit_upgrades_definition->boarding_seats[i].target_seat_index;
+
+					if (seat_index == target_seat_index)
+					{
+						datum_index boarding_unit = Objects::Units::GetUnitInSeat(parent_unit_index, boarding_seat_index);
+
+						// If the seat is being boarded, prevent ai from exiting
+						if (boarding_unit != datum_index::null)
+							return;
+					}
+				}
+			}
+
+			// Exit the vehicle like normal if conditions haven't been met
+			*unit->unit.animation.GetAnimationState() = Enums::_unit_animation_state_seat_exit;
+		}
+	};
+};
---
+++ OpenSauce/Halo1/Halo1_CE/Objects/Units.cpp
@@ -0,0 +1,200 @@
+/*
+	Yelo: Open Sauce SDK
+		Halo 1 (CE) Edition
+
+	See license\OpenSauce\Halo1_CE for specific license information
+*/
+#include "Common/Precompile.hpp"
+#include "Objects/Units.hpp"
+
+#include <blamlib/Halo1/game/game_globals.hpp>
+#include <blamlib/Halo1/units/unit_definitions.hpp>
+#include <blamlib/Halo1/models/model_animation_definitions.hpp>
+
+#include "Game/EngineFunctions.hpp"
+#include "Game/GameState.hpp"
+#include "Memory/MemoryInterface.hpp"
+#include "Networking/MDP.hpp"
+#include "Networking/MessageDeltas.hpp"
+#include "TagGroups/TagGroups.hpp"
+
+#include "TagGroups/project_yellow_definitions.hpp"
+
+namespace Yelo
+{
+	namespace Objects {
+		byte* s_unit_data::GetYeloGrenade2Count()
+		{
+			GameState::s_yelo_header_data& yelo_header = GameState::GameStateGlobals()->header->yelo;
+
+			return GameState::YeloGameStateEnabled() && yelo_header.unit_grenade_types_count >= Enums::_unit_grenade_type_yelo2+1
+				? &this->grenade_counts[Enums::_unit_grenade_type_yelo2] : NULL;
+		}
+		byte* s_unit_data::GetYeloGrenade3Count()
+		{
+			GameState::s_yelo_header_data& yelo_header = GameState::GameStateGlobals()->header->yelo;
+
+			return GameState::YeloGameStateEnabled() && yelo_header.unit_grenade_types_count >= Enums::_unit_grenade_type_yelo3+1
+				? &this->grenade_counts[Enums::_unit_grenade_type_yelo3] : NULL;
+		}
+
+		byte* s_unit_data::GetZoomLevel()
+		{
+			GameState::s_yelo_header_data& yelo_header = GameState::GameStateGlobals()->header->yelo;
+
+			return GameState::YeloGameStateEnabled() && yelo_header.unit_grenade_types_count > Enums::k_unit_grenade_types_count
+				? &this->zoom_level_yelo : &this->zoom_level;
+		}
+		byte* s_unit_data::GetDesiredZoomLevel()
+		{
+			GameState::s_yelo_header_data& yelo_header = GameState::GameStateGlobals()->header->yelo;
+
+			return GameState::YeloGameStateEnabled() && yelo_header.unit_grenade_types_count > Enums::k_unit_grenade_types_count
+				? &this->desired_zoom_level_yelo : &this->desired_zoom_level;
+		}
+	};
+
+	namespace Objects { namespace Units {
+#define __EL_INCLUDE_ID			__EL_INCLUDE_OBJECTS
+#define __EL_INCLUDE_FILE_ID	__EL_OBJECTS_UNITS
+#include "Memory/_EngineLayout.inl"
+#include "Objects/Units.Boarding.inl"
+#include "Objects/Units.Animations.inl"
+#include "Objects/Units.GrenadeCounts.inl"
+
+		void Initialize()
+		{
+			Animations::Initialize();
+			Boarding::Initialize();
+			
+			static const byte opcode_null[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+
+			Memory::WriteMemory(GET_FUNC_VPTR(BIPED_UPDATE_CHECK_PARENT_UNIT_TYPE), opcode_null, 6);
+		}
+
+		void Dispose()
+		{
+		}
+
+		void InitializeForNewMap()
+		{
+			GameState::s_yelo_header_data& yelo_header = GameState::GameStateGlobals()->header->yelo;
+			// handle the grenade types upgrading, if needed
+			if(	GameState::YeloGameStateEnabled() && 
+				yelo_header.flags.update_unit_grenade_types_count)
+			{
+				uint32 count = yelo_header.unit_grenade_types_count;
+
+				bool enabled = count > Enums::k_unit_grenade_types_count;
+
+				InitializeGrenadeCounts_UnitZoomLevelRefs(enabled);
+				InitializeGrenadeCounts_UnitDesiredZoomLevelRefs(enabled);
+				InitializeGrenadeCounts_NumberOfUnitGrenadeTypes(count);
+				InitializeGrenadeCounts_UnitGrenadeCounts(enabled);
+				InitializeGrenadeCounts_MessageDeltaGrenadeCounts(enabled);
+
+				yelo_header.flags.update_unit_grenade_types_count = false;
+			}
+		}
+
+		void DisposeFromOldMap()
+		{
+		}
+
+		void InitializeForYeloGameState(bool enabled)
+		{
+		}
+
+		int32 DefinitionFindUnitUpgradeBlockIndex(datum_index unit_tag_index)
+		{
+			int32 unit_external_upgrades_count = TagGroups::_global_yelo_globals->unit_external_upgrades.Count;
+
+			for(int32 x = 0; x < unit_external_upgrades_count; x++)
+			{
+				TagGroups::s_unit_external_upgrades const& unit_upgrade = TagGroups::_global_yelo_globals->unit_external_upgrades[x];
+
+				if(unit_tag_index == unit_upgrade.unit.tag_index)
+					return x;
+			}
+			return NONE;
+		}
+		
+		int32 DefinitionFindUnitUpgradeBoardingSeatBlockIndex(TagGroups::s_unit_external_upgrades const* unit_upgrade_definition,
+			int32 seat_index)
+		{
+			if (unit_upgrade_definition != NULL)
+			{
+				int32 boarding_seats_count = unit_upgrade_definition->boarding_seats.Count;
+
+				for(int32 x = 0; x < boarding_seats_count; x++)
+				{
+					if(seat_index == unit_upgrade_definition->boarding_seats[x].seat_index)
+						return x;
+				}
+			}
+			return NONE;
+		}
+
+		TagGroups::s_unit_external_upgrades const* DefinitionFindUnitUpgradesBlock(datum_index unit_index)
+		{
+			if (unit_index != datum_index::null)
+			{
+				// Check if a unit upgrades block exists in yelo_globals
+				if (TagGroups::_global_yelo_globals->unit_external_upgrades.Count > 0)
+				{
+					Objects::s_unit_datum* unit = (*Objects::ObjectHeader())[unit_index]->_unit;
+					datum_index unit_tag_index = unit->object.definition_index;
+					int32 unit_upgrade_index = Objects::Units::DefinitionFindUnitUpgradeBlockIndex(unit_tag_index);
+
+					// Check if an upgrades block exists for this unit
+					if (unit_upgrade_index != NONE)
+						return &TagGroups::_global_yelo_globals->unit_external_upgrades[unit_upgrade_index];
+				}
+			}
+			return NULL;
+		}
+
+		TagGroups::s_unit_boarding_seat const* DefinitionFindUnitUpgradesBoardingSeatBlock(datum_index unit_index, int16 seat_index)
+		{
+			if (unit_index != datum_index::null)
+			{
+				TagGroups::s_unit_external_upgrades const* unit_external_upgrades_definition = DefinitionFindUnitUpgradesBlock(unit_index);
+
+				// Check if a unit upgrades block exists in yelo_globals
+				if (unit_external_upgrades_definition != NULL)
+				{
+					int32 boarding_seat_block_index = 
+						DefinitionFindUnitUpgradeBoardingSeatBlockIndex(unit_external_upgrades_definition, seat_index);
+
+					// Check if boarding seat block exists for this seat index
+					if (boarding_seat_block_index != NONE)
+						return &unit_external_upgrades_definition->boarding_seats[boarding_seat_block_index];
+				}
+			}
+			return NULL;
+		}
+
+		datum_index GetUnitInSeat(datum_index vehicle_index, int32 seat_index)
+		{
+			s_unit_datum* vehicle = (*Objects::ObjectHeader())[vehicle_index]->_unit;
+			datum_index unit = datum_index::null;
+			datum_index next_object = vehicle->object.first_object_index;
+
+			for (;;)
+			{
+				if (next_object == datum_index::null) break;
+
+				int16 unit_seat_index = (*Objects::ObjectHeader())[next_object]->_unit->unit.vehicle_seat_index;
+				byte object_type = (*Objects::ObjectHeader())[next_object]->_object->type;
+
+				if (object_type == Yelo::Enums::_object_type_biped)
+					if (unit_seat_index == seat_index) unit = next_object;
+
+				next_object = (*Objects::ObjectHeader())[next_object]->_object->next_object_index;
+			}
+
+			return unit;
+		}
+
+	}; };
+};
